d668bc1581abd8dd5f80d5e6e295e1c1bed226ed
Emily-Huang499 <66002558+Emily-Huang499@users.noreply.github.com>
2020-08-21 16:46:17 +0800

    Update Pstudent.java

--- Pstudent.java
+++ Pstudent.java
@@ -14,38 +14,37 @@ class Person {
         this.name = name;
         this.age = age;
     }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
+    public String getName() { 
+	    return name; 
+    }
+    public void setName(String name) { 
+	    this.name = name;
+    }
+    public int getAge() {
+	    return age;
+    }
+    public void setAge(int age) {
+	    this.age = age;
+    }
 }
-
 class Student extends Person {
     protected int score;
-
     public Student(String name, int age, int score) {
         super(name, age);
         this.score = score;
     }
-
     public int getScore() { 
         return score;
     }
 }
 public class PrimaryStudent extends Student{
-	
 	protected int grade;
-	
 	public PrimaryStudent(String name, int age, int score, int grade) {
 		super(name, age, score);
 		this.grade = grade;
 	}
-	
 	public int getGrade() {
 		return grade;
 	}
-
 }
 
