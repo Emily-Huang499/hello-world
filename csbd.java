33b78d802183a1814bd1f1a453afcfa6baffefe4
Emily-Huang499 <278147797@qq.com>
2020-08-21 16:40:38 +0800

    基本类型参数绑定

---
+++ csbd.java
@@ -0,0 +1,22 @@
+```java
+public class csbd{
+    public static void main(String[] args){
+        Person p=new Person();
+        int n=15;
+        p.setAge(n);
+        System.out.println(p.getAge());
+        n=20;
+        System.out.println(p..getAge());
+    }
+}
+class Person{
+    private int age;
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+}
+```
+
