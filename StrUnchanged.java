e9a8398a566d656d58b50cc3f7ba867abcd37978
Emily-Huang499 <278147797@qq.com>
2020-08-29 18:23:39 +0800

    String的不变性

---
+++ StrUnchanged.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Arrays;
+
+public class StrUnchanged {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = new int[] { 88, 77, 51, 66 };
+        Score s = new Score(scores);
+        s.printScores();
+        scores[2] = 99;
+        s.printScores();
+    }
+}
+
+class Score {
+    private int[] scores;
+    public Score(int[] scores) {
+        this.scores = scores;
+    }
+
+    public void printScores() {
+        System.out.println(Arrays.toString(scores));
+    }
+}
+
