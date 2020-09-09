4f011ccf20141a20b9aa1b6d486e7e3bfc3293f8
Emily-Huang499 <278147797@qq.com>
2020-09-09 23:25:28 +0800

    readAsString()方法

---
+++ ReadAsStrMethod.java
@@ -0,0 +1,24 @@
+package seventhc;
+
+import java.io.*;
+
+public class ReadAsStrMethod {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		byte[] data = { 72, 101, 108, 108, 111, 33 };
+        try (InputStream input = new ByteArrayInputStream(data)) {
+            String s = readAsString(input);
+            System.out.println(s);
+        }
+    }
+
+    public static String readAsString(InputStream input) throws IOException {
+        int n;
+        StringBuilder sb = new StringBuilder();
+        while ((n = input.read()) != -1) {
+            sb.append((char) n);
+        }
+        return sb.toString();
+    }
+}
