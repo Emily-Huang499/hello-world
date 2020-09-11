b3d0218599e64d8b55b708ed1d5d6e09c5d1411e
Emily-Huang499 <278147797@qq.com>
2020-09-11 20:16:04 +0800

    timestampToString()方法

---
+++ TimeStampToStr.java
@@ -0,0 +1,22 @@
+package eighth;
+
+import java.time.*;
+import java.time.format.*;
+import java.util.Locale;
+
+public class TimeStampToStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long ts = 1574208900000L;
+        System.out.println(timestampToString(ts, Locale.CHINA, "Asia/Shanghai"));
+        System.out.println(timestampToString(ts, Locale.US, "America/New_York"));
+    }
+
+    static String timestampToString(long epochMilli, Locale lo, String zoneId) {
+        Instant ins = Instant.ofEpochMilli(epochMilli);
+        DateTimeFormatter f = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.SHORT);
+        return f.withLocale(lo).format(ZonedDateTime.ofInstant(ins, ZoneId.of(zoneId)));
+    }
+}
+
