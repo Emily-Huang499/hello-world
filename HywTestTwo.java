7f237ab2e291641724f5b99f801aff2d719fd58d
Emily-Huang499 <278147797@qq.com>
2020-09-13 17:18:12 +0800

    泛型练手(I still don't quite understand it.)

---
+++ HywTestTwo.java
@@ -0,0 +1,40 @@
+package tests;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+public class HywTestTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class<IntPair> clazz = IntPair.class;
+		Type t = clazz.getGenericSuperclass();
+		if (t instanceof ParameterizedType) {
+			ParameterizedType pt = (ParameterizedType) t;
+			Type[] types = pt.getActualTypeArguments();
+			Type firstType = types[0];
+			Class<?> typeClass = (Class<?>) firstType;
+		    System.out.println(typeClass);
+		}
+	}
+}
+class Pair<T>{
+	private T first;
+	private T last;
+	public Pair(T first, T last) {
+		this.first = first;
+		this.last = last;
+	}
+	public T getFirst() {
+		return first;
+	}
+	public T getLast() {
+		return last;
+	}
+}
+
+class IntPair extends Pair<Integer>{
+	public IntPair(Integer first, Integer last) {
+		super(first, last);
+	}
+}
