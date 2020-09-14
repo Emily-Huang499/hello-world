7b913007b1ba5fc8fdd2384f1f7168e009afbbab
Emily-Huang499 <278147797@qq.com>
2020-09-14 17:47:34 +0800

    正则表达式练习

---
+++ 正则表达式/RegexTestTwo.java
@@ -0,0 +1,25 @@
+package tenthc;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexTestTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pattern pattern = Pattern.compile("(\\d{3,4})\\-(\\d{7,8})");
+		System.out.println(pattern.matcher("010-12345678").matches());
+		System.out.println(pattern.matcher("021-123456").matches());
+		System.out.println(pattern.matcher("010#12345678").matches());
+		
+		Matcher matcher = pattern.matcher("010-12345678");
+		if (matcher.matches()) {
+			String whole = matcher.group(0);
+			String are = matcher.group(1);
+			String tel = matcher.group(2);
+			System.out.println(whole);
+			System.out.println(are);
+			System.out.println(tel);
+		}
+	}
+}
