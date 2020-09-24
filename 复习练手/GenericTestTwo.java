0f40c86ab66600b8ba0ccc79c09a38f56aafc63f
Emily-Huang499 <278147797@qq.com>
2020-09-24 23:43:17 +0800

    泛型复习练手

---
+++ 复习练手/GenericTestTwo.java
@@ -0,0 +1,27 @@
+package com.indi.generic.test;
+
+public class GenericTestTwo {
+
+	public static void main(String[] args) {
+		Pair<String> p1 = new Pair<>("Hello", "world");
+        Pair<Integer> p2 = new Pair<>(123, 456);
+        Class c1 = p1.getClass();
+        Class c2 = p2.getClass();
+        System.out.println(c1==c2);
+        System.out.println(c1==Pair.class);
+	}
+}
+class Pair<T> {
+    private T first;
+    private T last;
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+    public T getFirst() {
+        return first;
+    }
+    public T getLast() {
+        return last;
+    }
+}
