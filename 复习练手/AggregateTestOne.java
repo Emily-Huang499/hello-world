c5b065c777fbe5fb2d573c5f5f0aa06957a1e403
Emily-Huang499 <278147797@qq.com>
2020-09-25 23:59:16 +0800

    集合复习练手

---
+++ 复习练手/AggregateTestOne.java
@@ -0,0 +1,15 @@
+package com.indi.aggregate.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AggregateTestOne {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+        list.add("apple"); // size=1
+        list.add("pear"); // size=2
+        list.add("apple"); // 允许重复添加元素，size=3
+        System.out.println(list.size());
+    }
+}
