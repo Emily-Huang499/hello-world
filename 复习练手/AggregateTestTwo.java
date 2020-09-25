312a2f97d5a5439d50da3fc4b16262cf402524a7
Emily-Huang499 <278147797@qq.com>
2020-09-26 00:00:21 +0800

    集合复习练手

---
+++ 复习练手/AggregateTestTwo.java
@@ -0,0 +1,24 @@
+package com.indi.aggregate.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AggregateTestTwo {
+
+	public static void main(String[] args) {
+		final int start = 10;
+        final int end = 20;
+        List<Integer> list = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            list.add(i);
+        }
+        int removed = list.remove((int) (Math.random() * list.size()));
+        int found = findMissingNumber(start, end, list);
+        System.out.println(list.toString());
+        System.out.println("missing number: " + found);
+        System.out.println(removed == found ? "测试成功" : "测试失败");
+    }
+    static int findMissingNumber(int start, int end, List<Integer> list) {
+        return 0;
+    }
+}
