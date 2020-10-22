5a33f3c5e822d1d6eda0ce49f7ec23cad7e0e80d
Emily-Huang499 <66002558+Emily-Huang499@users.noreply.github.com>
2020-10-22 13:36:38 +0800

    jdbc执行删除操作

--- jdbc练习/JdbcTest02.java
+++ jdbc练习/JdbcTest02.java
@@ -1,7 +1,43 @@
 package com.emilytest.util;
 
+import java.sql.*;
+
 public class JdbcTest02 {
     public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt =null;
+
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emilytest?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC","root","1130");
+
+            stmt = conn.createStatement();
+
+            String sql = "delete from dept where deptno = 10";
+
+            int count = stmt.executeUpdate(sql);
+
+            System.out.println(count == 1? "删除成功": "删除失败");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null){
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
