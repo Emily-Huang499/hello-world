9b9c24203cdb02836b37142d6155daa6768b462e
Emily-Huang499 <66002558+Emily-Huang499@users.noreply.github.com>
2020-10-22 23:03:44 +0800

    用户登录功能界面的初始化

---
+++ jdbc练习/JdbcTest07.java
@@ -0,0 +1,33 @@
+package com.emilytest.util;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class JdbcTest07 {
+    public static void main(String[] args) {
+        Map<String,String> userLoginInfo = initUI();
+        boolean loginSuccess = login(userLoginInfo);
+        System.out.println(loginSuccess ? "登录成功" : "登录失败");
+    }
+
+    private static boolean login(Map<String, String> userLoginInfo) {
+        return false;
+    }
+
+    private static Map<String, String> initUI() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("用户名：");
+        String LoginName = s.next();
+
+        System.out.print("密码：");
+        String Loginpwd = s.next();
+
+        Map<String,String> userLoginInfo = new HashMap<>();
+        userLoginInfo.put("LoginName",LoginName);
+        userLoginInfo.put("Loginpwd",Loginpwd);
+
+        return userLoginInfo;
+    }
+}
