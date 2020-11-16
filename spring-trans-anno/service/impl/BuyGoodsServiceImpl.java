fcde1db2fe300dca7ea7d470c6ffd975f53a5dd6
Emily-Huang499 <66002558+Emily-Huang499@users.noreply.github.com>
2020-11-17 00:00:22 +0800

    spring事务相关注解

---
+++ spring-trans-anno/service/impl/BuyGoodsServiceImpl.java
@@ -0,0 +1,60 @@
+package com.emily.service.impl;
+
+import com.emily.dao.GoodsDao;
+import com.emily.dao.SaleDao;
+import com.emily.domain.Goods;
+import com.emily.domain.Sale;
+import com.emily.service.BuyGoodsService;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+public class BuyGoodsServiceImpl implements BuyGoodsService {
+    private SaleDao saleDao;
+    private GoodsDao goodsDao;
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            isolation = Isolation.DEFAULT,
+            readOnly = false,
+            rollbackFor = {
+                    NullPointerException.class,
+                    RuntimeException.class
+            }
+    )
+    @Override
+    public void buy(Integer goodsId, Integer nums) {
+        System.out.println("=====buy方法的开始======");
+        Sale sale = new Sale();
+        sale.setGid(goodsId);
+        sale.setNums(nums);
+        saleDao.insertSale(sale);
+
+        Goods goods = goodsDao.selectGoods(goodsId);
+        if (goods == null){
+            throw new NullPointerException("编号是" + goodsId+"，商品不存在");
+        }else if (goods.getAmount() < nums){
+            throw new RuntimeException("编号是" + goodsId+"，商品库存不足");
+        }
+        Goods buyGoods = new Goods();
+        buyGoods.setId(goodsId);
+        buyGoods.setAmount(nums);
+        goodsDao.updateGoods(buyGoods);
+        System.out.println("=====buy方法的完成======");
+    }
+
+    public SaleDao getSaleDao() {
+        return saleDao;
+    }
+
+    public void setSaleDao(SaleDao saleDao) {
+        this.saleDao = saleDao;
+    }
+
+    public GoodsDao getGoodsDao() {
+        return goodsDao;
+    }
+
+    public void setGoodsDao(GoodsDao goodsDao) {
+        this.goodsDao = goodsDao;
+    }
+}
