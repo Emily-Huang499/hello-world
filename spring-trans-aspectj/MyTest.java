cda6a0c648d1b5b68330bc6398cfa48d9dedcfb0
Emily-Huang499 <66002558+Emily-Huang499@users.noreply.github.com>
2020-11-17 00:02:41 +0800

    spring使用aspectj处理事务

---
+++ spring-trans-aspectj/MyTest.java
@@ -0,0 +1,16 @@
+package com.emily;
+
+import com.emily.service.BuyGoodsService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void test01(){
+        String config = "applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        BuyGoodsService service = (BuyGoodsService) ac.getBean("buyGoodsService");
+        service.buy(1002,10);
+    }
+}
